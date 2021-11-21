639339c436e8e482a147dfdeef197d383e2768fa
Ahmad Faiz Bin Besah @ Sitam <afaizbesah@gmail.com>
2021-11-21 16:10:09 +0800

    Create SafasSystem2ApplicationTests.java

---
+++ Safas-System-2/src/test/java/com/sitam/safas/SafasSystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitam.safas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafasSystem2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
