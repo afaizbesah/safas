7862063f463033f78bfa6f4cd076eab3cb61e15f
Ahmad Faiz Bin Besah @ Sitam <afaizbesah@gmail.com>
2021-11-21 16:10:15 +0800

    Create SafasSystem2Application.java

---
+++ Safas-System-2/src/main/java/com/sitam/safas/SafasSystem2Application.java
@@ -0,0 +1,13 @@
+package com.sitam.safas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafasSystem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafasSystem2Application.class, args);
+	}
+
+}
